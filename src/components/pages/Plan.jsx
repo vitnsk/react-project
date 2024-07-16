35b5bb19f71778ce23a64f113de5b9076254874b
vitnsk <121999199+vitnsk@users.noreply.github.com>
2024-07-16 21:38:02 +0700

    useSearchParams

--- src/components/pages/Plan.jsx
+++ src/components/pages/Plan.jsx
@@ -1,18 +1,21 @@
 import React, {useEffect} from 'react';
-import{useParams} from 'react-router-dom';
+import{useParams, useSearchParams} from 'react-router-dom';
 
 const Plan =()=>{
 const {demoParam}= useParams()
+const [params,setParams]=useSearchParams();
+
 useEffect(()=>{
     console.log(demoParam)
-},[demoParam]
+    setParams({ userName: 'Nik', userAge: '29'})
+},[demoParam, setParams]
 )
 
 return(
     <>
 
-    <span style={{display: 'block', marginTop: '100px', marginLeft: '100px'}}>страница планирования</span>
-        
+    <span style={{display: 'block', marginTop: '100px', marginLeft: '100px'}}>имя пользователя-{params.get('userName')}</span>
+    <span style={{display: 'block', marginTop: '100px', marginLeft: '100px'}}>имя пользователя-{params.get('userAge')}</span>
     </>
 );
 
