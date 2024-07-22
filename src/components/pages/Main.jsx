a78b01e03a3f2b1e01fd75fc3ad309786b376b9c
vitnsk <121999199+vitnsk@users.noreply.github.com>
2024-07-22 22:33:42 +0700

    useRef_useContext

--- src/components/pages/Main.jsx
+++ src/components/pages/Main.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { changeViewType,changeComment,changeValue } from '../../redux-store/redusers/view-type-for-main';
 import { useSelector,useDispatch } from 'react-redux';
+import FooterContext from '../../redux-store/context/footerContext'
 import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
@@ -12,124 +13,166 @@ import InputComponent from '../comps/Input';
 
 import css from '../../styles/form.css'
 
+import { useRef } from 'react'
+//import useNumberValueFormat from '../../hooks/useNumberValueFormat'
 
+const { FormContainer, Button, Input } = css
 
-const { FormContainer, Button } = css;
+const Main = (props) => {
 
-// React.createElement(
-//     'input',{placeholder: 'Введите сумму транзакции'}, null
-// )
-const Main =(props)=>{
+  const { action } = props
 
-const {action}=props
+  const valueInput = useRef()
+  const [ footerText, setFooterText] = useState('Новый курс по основам ReactJS 2023')
+ // const [ formatValue, formating ] = useNumberValueFormat()
 
-//const [value, setValue]=useState('')
-//const [type, setType]=useState('доход')
-//const [comment, setComment]=useState('')
+  const dispatch = useDispatch()
+  const viewType = useSelector(state => state.viewTypeMain.viewType)
+  const viewValue = useSelector(state => state.viewTypeMain.value)
+  const viewComment = useSelector(state => state.viewTypeMain.comment)
 
-const dispatch=useDispatch()
-const viewType=useSelector(state=>state.viewTypeMain.viewType)
-const viewValue=useSelector(state=>state.viewTypeMain.value)
-const viewComment=useSelector(state=>state.viewTypeMain.comment)
+  //const validation = () => {
+    // if ( formatValue.length > 2 && viewType ) {
+    //   console.log('валидация прошла успешно')
 
-
-const validation=()=>{
-if(viewValue.length > 2 && viewType){
-    console.log('Валидация прошла успешно')
-
-const dataLine = `${viewValue}::${viewType}::${viewComment}`
-
-// setData(
-//     prev=>{
-//         let newData=prev
-//         newData.push(dataLine)
-//         return newData
-//     }
-// )
-
-
-action( dataLine )
-
-    dispatch(changeValue(''))
-   dispatch(changeViewType('доход'))
-   dispatch(changeComment(''))
-}
-    else console.log('Ошибка валидации')
-
-
-}
-const handleChange = (event) => {
-    dispatch(changeViewType(event.target.value));
-  };
-
-  const handleChangeValue = (param) => {
-    dispatch(changeValue(param));
-  };
-
-  const handleChangeComment = (param) => {
-    dispatch(changeComment(param));
-  };
-
-  const handleChangeCommentRadio = (event) => {
-    dispatch(changeComment(event.target.value));
-  };
-
-  useEffect(()=>{console.log(viewType)}, [viewType])
-return(
-    <>
+    //   const dataLine = `${formatValue}::${viewType}::${viewComment}`
     
-<FormContainer style={{alignItems: 'flex-start'}}>
-   <InputComponent inputValue={viewValue} action={handleChangeValue} placeholder={"Введите сумму транзакции"} maxLength={"100"}/>
-   <FormControl style={{marginTop:'9px',marginBottom: '12px'}}>
-      <FormLabel id="demo-controlled-radio-buttons-group">Введите тип транзакции</FormLabel>
-      <RadioGroup
-        aria-labelledby="demo-controlled-radio-buttons-group"
-        name="controlled-radio-buttons-group"
-        value={viewType}
-        onChange={handleChange}
-        style={{marginTop:'5px',marginLeft: '6px'}}
-      >
-        <FormControlLabel value="расход" control={<Radio />} label="Расход" />
-        <FormControlLabel value="доход" control={<Radio />} label="Доход" />
-      </RadioGroup>
-    </FormControl>
+      const validation=()=>{
+        if(viewValue.length > 2 && viewType){
+            console.log('Валидация прошла успешно')
+        
+        const dataLine = `${viewValue}::${viewType}::${viewComment}`
+      action(dataLine)
 
-   {/* <InputComponent inputValue={type} action={setType}  placeholder={"Введите тип транзакции"}/> */}
-  {viewType==='доход' && <InputComponent inputValue={viewComment} action={handleChangeComment} placeholder={"Введите комментарий"}/>}
-  {viewType==='расход' && <FormControl style={{marginTop:'0px',marginBottom: '14px'}}>
-      <FormLabel id="demo-controlled-radio-buttons-group">Введите тип расходов</FormLabel>
-      <RadioGroup
-        aria-labelledby="demo-controlled-radio-buttons-group"
-        name="controlled-radio-buttons-group"
-        value={viewComment}
-        onChange={handleChangeCommentRadio}
-        style={{marginTop:'5px',marginLeft: '6px'}}
-      >
-        <FormControlLabel value="покупка продуктов" control={<Radio />} label="Покупка продуктов" />
-        <FormControlLabel value="оплата счетов" control={<Radio />} label="Олата счетов" />
-        <FormControlLabel value="покупка одежды" control={<Radio />} label="Покупка одежды" />
-        <FormControlLabel value="расходы на транспорт" control={<Radio />} label="Расходы на транспорт" />
-        <FormControlLabel value="развлечения" control={<Radio />} label="Развлечения" />
-        <FormControlLabel value="путешествия" control={<Radio />} label="Путешествия" />
-      </RadioGroup>
-    </FormControl>}
+      dispatch(changeValue(''))
+      dispatch(changeViewType('доход'))
+      dispatch(changeComment(''))
 
+      fetch('/add-data', {
+        method: 'POST',
+        headers: { "Content-type": "application/json; charset=UTF-8" }, 
+        body: JSON.stringify({
+          value: dataLine
+        })
+      })
 
-   <Button
-   backgroundColor={
-    viewValue.length < 3 ?
-    "rgb(229, 229, 229)" :
-     viewType.length < 3 ?
-     "rgb(229, 229, 229)" :
-    "rgb(176, 243, 71)" 
+    } else console.log('ошибка валидации')
+  }
+
+  const handleChange = (event) => {
+    dispatch(changeViewType(event.target.value));
+  }
+  const handleChangeValue = (param) => {
+    dispatch(changeValue(param))
+  }
+  const handleChangeComment = (param) => {
+    dispatch(changeComment(param))
+  }
+  const handleChangeCommentRadio = (event) => {
+    dispatch(changeComment(event.target.value))
+  }
+
+  const setFocus = () => { 
+    valueInput.current.disabled = false
+    valueInput.current.focus()
+  }
+
+  useEffect(() => { console.log(viewType) }, [viewType])
+  //useEffect(() => { console.log(formatValue) }, [formatValue])
+
+  return (
+    <React.Fragment>
+      <FormContainer style={{ alignItems: 'flex-start' }}>
+
+        {/* ----------------------------------------- */}
+        {/* react useRef */}
+        {/* ----------------------------------------- */}
+        
+        <Button 
+          backgroundColor={"rgb(176, 243, 71)"}
+          onClick={setFocus}
+          style={{ marginBottom: '12px' }}
+        >
+          Начать заполнение
+        </Button>
+        <Input
+          ref={valueInput}
+          value={viewValue}
+          type={"text"}
+          placeholder={"Введите сумму транзакции"}
+          maxLength={"100"}
+          disabled
+          onChange={event => {
+            const newValue = event.target.value
+           // formating(newValue)
+            handleChangeValue(newValue)
+          }}
+        />
+
+        {/* ----------------------------------------- */}
+        {/* react useRef */}
+        {/* ----------------------------------------- */}
+        
+        { false && <InputComponent ref={valueInput} inputValue={viewValue} action={handleChangeValue} placeholder={"Введите сумму транзакции"}/> }
+        
+        <FormControl style={{ marginTop: '9px', marginBottom: '12px' }}>
+          <FormLabel id="demo-controlled-radio-buttons-group">Выберите тип транзакции</FormLabel>
+          <RadioGroup
+            aria-labelledby="demo-controlled-radio-buttons-group"
+            name="controlled-radio-buttons-group"
+            value={viewType}
+            onChange={handleChange}
+            style={{ marginTop: '5px', marginLeft: '6px' }}
+          >
+            <FormControlLabel value="расход" control={<Radio />} label="Расход" />
+            <FormControlLabel value="доход" control={<Radio />} label="Доход" />
+          </RadioGroup>
+        </FormControl>
+        { viewType === 'доход' && <InputComponent inputValue={viewComment} action={handleChangeComment} placeholder={"Введите комментарий"}/> }
+        { viewType === 'расход' && <FormControl style={{ marginTop: '0px', marginBottom: '14px' }}>
+          <FormLabel id="demo-controlled-radio-buttons-group">Выберите тип расходов</FormLabel>
+          <RadioGroup
+            aria-labelledby="demo-controlled-radio-buttons-group"
+            name="controlled-radio-buttons-group"
+            value={viewComment}
+            onChange={handleChangeCommentRadio}
+            style={{ marginTop: '5px', marginLeft: '6px' }}
+          >
+            <FormControlLabel value="покупка продуктов" control={<Radio />} label="Покупка продуктов" />
+            <FormControlLabel value="оплата счетов" control={<Radio />} label="Оплата счетов" />
+            <FormControlLabel value="покупка одежды" control={<Radio />} label="Покупка одежды" />
+            <FormControlLabel value="расходы на транспорт" control={<Radio />} label="Расходы на транспорт" />
+            <FormControlLabel value="развлечения" control={<Radio />} label="Развлечения" />
+            <FormControlLabel value="путешествия" control={<Radio />} label="Путешествия" />
+          </RadioGroup>
+        </FormControl> }
+        <Button 
+          backgroundColor={
+            viewValue.length < 3     ?
+            "rgb(229, 229, 229)" :
+            viewType.length < 3      ? 
+            "rgb(229, 229, 229)" :
+            "rgb(176, 243, 71)"
+          }
+          onClick={validation}
+        >Сохранить транзакцию</Button>
+      </FormContainer>
+       { true && <FooterContext.Provider value={[ footerText,
+         setFooterText
+          ]}>
+        
+        <Food></Food>
+      
+      </FooterContext.Provider> }
+      { false && <FooterContext.Provider value={footerText}>
+        <FooterContext.Consumer>
+
+          { value => <Food>{ value }</Food> }
+
+        </FooterContext.Consumer>
+      </FooterContext.Provider> } 
+    </React.Fragment>
+  )
 }
-   onClick={validation}
-   >Сохранить транзакцию</Button>
-</FormContainer>
 
-    <Food></Food>
-    </>
-);
-
-}
-export default Main;
+export default Main
--- src/components/views/global/Food.jsx
+++ src/components/views/global/Food.jsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import React,{useContext} from 'react';
+import FooterContext from '../../../redux-store/context/footerContext'
 import css from '../../../styles/styles.css';
 
 const {FooterContainer}=css;
 
 
-const Food =()=>{
-return(
-    <>
-    <FooterContainer></FooterContainer>
-    </>
-);
+const Food = ({ children }) => {
 
-}
+    // eslint-disable-next-line no-unused-vars
+    const [ footerText, setFooterText ] = useContext(FooterContext)
+  
+    return (
+      <React.Fragment>
+        <FooterContainer 
+              onClick={() => setFooterText('Новый текст для подвала приложения')} 
+              style={{ justifyContent: 'space-around', fontSize: '14px' }}
+            >
+              { footerText }
+            </FooterContainer>
+        {/* <FooterContainer style={{ justifyContent: 'space-around', fontSize: '14px' }}>{footerText}</FooterContainer> */}
+        {/* <FooterContainer style={{ justifyContent: 'space-around', fontSize: '14px' }}>{children}</FooterContainer> */}
+      </React.Fragment>
+    )
+  }
+  
+  
 export default Food;
